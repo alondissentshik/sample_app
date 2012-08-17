2f410b1c13ed96487a4ddaa30a878e259c2fab77
alon d <alon.dissentshik@gmail.com>
2012-08-17 23:14:20 +0300

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
 	def home
+		if signed_in?
+			@micropost  = current_user.microposts.build
+			@feed_items = current_user.feed.paginate(page: params[:page])
+		end
 	end
 
 	def help
@@ -9,5 +13,5 @@ class StaticPagesController < ApplicationController
 	end
 
 	def contact
-  	end
+	end
 end
